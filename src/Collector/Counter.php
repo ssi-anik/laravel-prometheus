8abe20ba39102d1ff15e85d235ac306b0ffb452d
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2023-03-12 09:35:04 +0600

    data is stored in the storage

--- src/Collector/Counter.php
+++ src/Collector/Counter.php
@@ -4,20 +4,33 @@ namespace Anik\Laravel\Prometheus\Collector;
 
 final class Counter extends Collector
 {
-    public static function forRequest(): self
+    /** @var int|float */
+    protected $count = 1;
+
+    public function increment($count = 1): self
     {
-        return self::create(config('prometheus.request.count.name'))
-                   ->setNamespace(config('prometheus.namespace') ?? '');
+        $this->count = $count;
+
+        return $this;
     }
 
-    public static function forDatabase(): self
-    {
-        return self::create(config('prometheus.database.count.name'))
-                   ->setNamespace(config('prometheus.namespace') ?? '');
-    }
-
+    /**
+     * @throws \Prometheus\Exception\MetricsRegistrationException
+     */
     public function store(): bool
     {
+        $keys = array_keys($this->labels);
+        $values = array_values($this->labels);
+
+        $this->getCollectorRegistry()
+             ->getOrRegisterCounter(
+                 $this->getNamespace(),
+                 $this->getName(),
+                 $this->getHelpText(),
+                 $keys
+             )
+             ->incBy($this->count, $values);
+
         return true;
     }
 }
