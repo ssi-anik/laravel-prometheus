f221eafeb9d8496159b5ffe62cdbea5b82868a25
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2023-03-26 23:57:55 +0600

    Request KV data extractor updated

--- src/Extractors/LumenRequest.php
+++ src/Extractors/LumenRequest.php
@@ -6,11 +6,6 @@ class LumenRequest extends Request
 {
     protected function getRouteAction(): array
     {
-        $route = $this->request->route();
-        if ($route && ($route[1] ?? null)) {
-            return $route[1];
-        }
-
-        return [];
+        return $this->request->route()[1] ?? [];
     }
 }
--- src/Extractors/Request.php
+++ src/Extractors/Request.php
@@ -23,21 +23,40 @@ class Request implements Arrayable
         return $this->request->route()->getAction();
     }
 
+    protected function routeAs(array $action): ?string
+    {
+        return $action['as'] ?? null;
+    }
+
+    protected function routeUses(array $action): ?string
+    {
+        /**
+         * Laravel always pushes `uses` as route action even if it's a closure
+         *
+         * Lumen pushes `uses` as route action
+         * .... if handler is string
+         * .... else if explicitly specified by the developer
+         *
+         * In Lumen, unless `uses` is specified, in most cases $action[0] is handling the route, and it's a closure.
+         * Otherwise, developer is trying any hack which IDK! 🤷🤷
+         *
+         */
+
+        $uses = $action['uses'] ?? $action[0] ?? null;
+        if ($uses instanceof Closure) {
+            $uses = 'Closure';
+        }
+
+        return is_null($uses) ? null : implode('@', Arr::wrap($uses));
+    }
+
     public function toArray(): array
     {
         $action = $this->getRouteAction();
-        $as = $action['as'] ?? null;
-        if ($uses = $action['uses'] ?? null) {
-            if ($uses instanceof Closure) {
-                $uses = 'Closure';
-            }
-
-            $uses = implode('@', Arr::wrap($uses));
-        }
 
         return [
             ($this->mapper['method'] ?? 'method') => $this->request->method(),
-            ($this->mapper['url'] ?? 'url') => $as ?? $uses ?? $this->request->path(),
+            ($this->mapper['url'] ?? 'url') => $this->routeAs($action) ?? $this->routeUses($action) ?? $this->request->path(),
         ];
     }
 }
